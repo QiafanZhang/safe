90f73480ea9144d462085b3d25740de582e01552
zhang <1248545203@qq.com>
2017-12-01 00:12:55 +0800

    commit SendActivity

---
+++ src/main/java/com/example/app2/SendActivity.java
@@ -0,0 +1,13 @@
+package com.example.app2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SendActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send);
+    }
+}
